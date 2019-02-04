737a349a4ebd9a599a9b18309759f46d01ba0e9f
trey <tcgtreyster509@gmail.com>
2019-02-04 12:18:36 -0800

    AA

---
+++ treyster509v2git.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  treyster509v2;
+git config --global user.name  trey;
+git config --global user.user treyster509v2;
+git config --global user.email tcgtreyster509@gmail.com;
+git config --list;
